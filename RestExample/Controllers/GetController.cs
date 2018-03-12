8e9696fdcf8ed11d062e33c4ea9a0449151f9506
Sven <sinodaurus@gmail.com>
2018-03-12 20:18:03 +0100

    Added /get

---
+++ RestExample/Controllers/GetController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestExample.Controllers
+{
+    public class GetController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<Models.Post> students = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+                //HTTP GET
+                var responseTask = client.GetAsync("posts");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<Models.Post>>();
+                    readTask.Wait();
+
+                    students = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    students = Enumerable.Empty<Models.Post>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(students);
+        }
+    }
+}
